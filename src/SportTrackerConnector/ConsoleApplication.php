2fd46bca59843dcde11b99027f7dd1105f430eed
Dragos Protung <dragos@protung.ro>
2014-08-07 23:43:22 +0200

    Updated the version number.

--- src/SportTrackerConnector/ConsoleApplication.php
+++ src/SportTrackerConnector/ConsoleApplication.php
@@ -19,7 +19,7 @@ class ConsoleApplication extends Application
      */
     public function __construct()
     {
-        parent::__construct('Sport tracker connector', '0.2');
+        parent::__construct('Sport tracker connector', '0.2.2');
 
         $this->add(new Upload());
         $this->add(new UploadSync());
